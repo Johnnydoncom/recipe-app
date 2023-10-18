05acd157cd1e0c0b88c9e7af5bb388b444a0a002
Johnnydoncom <johnnydoncom@yahoo.com>
2023-10-18 10:48:16 +0100

    readme commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Artisan::call('currency:manage add ngn,usd');
-        Artisan::call('app:update-currency');
+        Artisan::call('update-currencies');
         currency()->update(env('DEFAULT_CURRENCY'), [
             'exchange_rate' => 1
         ]);
